974e53445caa96ede449dc45b4612145e81321e1
刘祥超 <iwind.liu@gmail.com>
2024-05-15 08:32:45 +0800

    优化WAF分组创建和修改弹窗尺寸

--- web/views/@default/servers/server/settings/waf/group.js
+++ web/views/@default/servers/server/settings/waf/group.js
@@ -32,7 +32,7 @@ Tea.context(function () {
 	// 更改分组
 	this.updateGroup = function (groupId) {
 		teaweb.popup("/servers/components/waf/updateGroupPopup?groupId=" + groupId, {
-			height: "16em",
+			height: "20em",
 			callback: function () {
 				teaweb.success("保存成功", function () {
 					window.location.reload()
